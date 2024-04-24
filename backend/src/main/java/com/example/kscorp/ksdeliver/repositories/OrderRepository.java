d5ef9dc732c651ca0a74427d1e6e4914cb4deb53
k-Sidney <kleubersidney@hotmail.com>
2024-04-23 23:36:10 -0300

    GET /orders

--- backend/src/main/java/com/example/kscorp/ksdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/example/kscorp/ksdeliver/repositories/OrderRepository.java
@@ -1,9 +1,15 @@
 package com.example.kscorp.ksdeliver.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.kscorp.ksdeliver.entities.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+	@Query("SELECT DISTINCT obj FROM Order obj JOIN FETCH obj.products"
+			+ " WHERE obj.status = 0 ORDER BY obj.moment ASC")
+	List<Order> findOrderWithProducts();
 }
---
+++ backend/src/main/java/com/example/kscorp/ksdeliver/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.example.kscorp.ksdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.kscorp.ksdeliver.dto.OrderDTO;
+import com.example.kscorp.ksdeliver.entities.Order;
+import com.example.kscorp.ksdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findOrderWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+}
